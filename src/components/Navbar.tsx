faea5ad845e51f58c0c745e357b5321543adde13
Arutowk <wwk199375@gmail.com>
2023-03-24 18:59:55 +0800

    完成展示3d电脑模型

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { logo, menu, close } from "../assets";
 import { navLinks } from "../constants";
-import "../mixin.css";
+import { styles } from "../styles";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -11,7 +11,13 @@ const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
 
   return (
-    <nav className="paddingX w-full flex items-center py-5 fixed top-0 z-20 bg-primary">
+    <nav
+      className={`${
+        styles.paddingX
+      } w-full flex items-center py-5 fixed top-0 z-20 ${
+        scrolled ? "bg-primary" : "bg-transparent"
+      }`}
+    >
       <div className=" w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to={"/"}
---
+++ src/utils/useMediaQuery.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export default function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const matchQueryList = window.matchMedia(query);
+    function handleChange(e: {
+      matches: boolean | ((prevState: boolean) => boolean);
+    }) {
+      setMatches(e.matches);
+    }
+    matchQueryList.addEventListener("change", handleChange);
+    return () => {
+      matchQueryList.removeEventListener("change", handleChange);
+    };
+  }, [query]);
+  return matches;
+}
